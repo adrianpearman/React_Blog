c2eb24d9ede56978c4ed6dc86cc0906d79619cad
Adrian Pearman <adrianpearman12@gmail.com>
2017-10-14 14:02:33 -0400

    updates to boiler plate files. unneccasry files were removed. style, actions, components and reducer folders added for scaffolding

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+
+<div>
+    logogog
+</div>
+
+  , document.getElementById('root'));
 registerServiceWorker();
